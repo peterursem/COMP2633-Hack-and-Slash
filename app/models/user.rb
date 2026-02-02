638ed601927389f785d2f1c9ca4b2cfb526d9433
David Caudron <dcaud547@mtroyal.ca>
2026-02-02 15:59:15 -0700

    Initial commit of flashcard game rails app with working user signup and login

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :username,
+             presence: true,
+             length: { minimum: 3, maimum: 20 },
+             uniqueness: { case_sensitive: false }
+
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false }
+end
